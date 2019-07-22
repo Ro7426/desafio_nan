c55389af366dcb44f93a62ff4c7e69d0ee0f10d8
Rodrigo Gonzalez Hernandez <jordan23rodrigo@gmail.com>
2019-07-22 01:58:40 -0400

    scaffold  person

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_054555) do
+ActiveRecord::Schema.define(version: 2019_07_22_055721) do
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "ocupation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
