a303f660f8ed4ce1f4335510df00bad4b583c478
Rodrigo Gonzalez Hernandez <jordan23rodrigo@gmail.com>
2019-07-22 01:51:28 -0400

    controlador project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_053542) do
+ActiveRecord::Schema.define(version: 2019_07_22_054555) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "budget"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
